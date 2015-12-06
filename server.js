5c0b1d919b8bb5e2c3b92b3b797c7a1e990d9c52
ZheyuJin <jin.zh@husky.neu.edu>
2015-12-06 01:57:58 -0800

    change in server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 #!/bin/env node
 //  OpenShift sample Node application
 var express = require('express');
-var fs      = require('fs');
-
+var fs = require('fs');
 
 var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
