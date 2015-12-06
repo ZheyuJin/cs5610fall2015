9560d18647ef01493e0396e739f9b69b18cdfda1
ZheyuJin <jin.zh@husky.neu.edu>
2015-12-06 01:55:27 -0800

    change server.js

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -1,14 +1,17 @@
 module.exports = function (app) {
-
-    /*models */
-    var userModel = require("./models/user.model.js")(app);
-    var formModel = require("./models/form.model.js")(app);
-
-
-    /*pass models to services*/
-    var userService = new require("./services/user.service.js")(app, userModel, null);
-
-    var formService = require("./services/form.service.js")(app, formModel, null);
-
-    var fieldService = require("./services/field.service.js")(app, formModel, null);
+    
+    var userModelModule = require("./models/user.model.js");
+    var userModel = new userModelModule(app);
+    
+    var formModelModule = require("./models/form.model.js");
+    var formModel = new formModelModule(app);
+    
+    var userServiceModule = require("./services/user.service.js");
+    var userService = new userServiceModule(app, userModel, null);
+    
+    var formServiceModule = require("./services/form.service.js");
+    var formService = new formServiceModule(app, formModel, null);
+    
+    var fieldServiceModule = require("./services/field.service.js");
+    var fieldService = new fieldServiceModule(app, formModel, null);
 };
